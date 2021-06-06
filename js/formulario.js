61e2806aa14ecfe28039b6fab925ee52396a69e2
Rebeca5114 <85424949+Rebeca5114@users.noreply.github.com>
2021-06-05 22:56:51 -0500

    Add files via upload

---
+++ js/formulario.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector("#MENU");
+let toggleIicon= document.querySelector("toggle-icon");
+
+toggleIicon.addEventListener("click", function(){
+    menu.classList.toggle("MENU-on");
+}
+);
